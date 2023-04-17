97be9c4947cf0d7de2def7614b36e279ac333bbe
BuildTools <cephas8080@outlook.com>
2023-04-17 21:34:53 +0100

    some funny animation

--- src/components/WebCom.svelte
+++ src/components/WebCom.svelte
@@ -95,5 +95,14 @@ animation: opacity 0.6s ease-in forwards;
         background: rgb(42,100,192);
         background: radial-gradient(circle, rgba(42,100,192,1) 0%, rgba(255,255,243,1) 0%, rgba(246,255,240,1) 48%, rgba(255,239,254,1) 100%); 
         border-color: dimgrey;
+        animation: forwards 400ms moveopen;
+    }
+    @keyframes moveopen {
+        0%{
+            transform: scale(0.1,0.1) rotate(180deg);
+        }
+        100%{
+            transform: scale(1,1);
+        }
     }
     </style>
